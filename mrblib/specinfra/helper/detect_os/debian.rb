8ff7fa51efed2830326ad072b716ba3881c5e0da
Takumi Takahashi <takumiiinn@gmail.com>
2025-04-26 11:56:29 +0900

    update specinfra to v2.94.0 from v2.91.0

--- mrblib/specinfra/helper/detect_os/debian.rb
+++ mrblib/specinfra/helper/detect_os/debian.rb
@@ -1,26 +1,52 @@
 class Specinfra::Helper::DetectOs::Debian < Specinfra::Helper::DetectOs
   def detect
     if (debian_version = run_command('cat /etc/debian_version')) && debian_version.success?
-      distro  = nil
-      release = nil
-      if (lsb_release = run_command("lsb_release -ir")) && lsb_release.success?
+      distro   = nil
+      release  = nil
+      codename = nil
+      if (lsb_release = run_command("lsb_release -irc")) && lsb_release.success?
         lsb_release.stdout.each_line do |line|
-          distro  = line.split(':').last.strip if line =~ /^Distributor ID:/
-          release = line.split(':').last.strip if line =~ /^Release:/
+          distro   = line.split(':').last.strip if line =~ /^Distributor ID:/
+          release  = line.split(':').last.strip if line =~ /^Release:/
+          codename = line.split(':').last.strip if line =~ /^Codename:/
         end
       elsif (lsb_release = run_command("cat /etc/lsb-release")) && lsb_release.success?
         lsb_release.stdout.each_line do |line|
-          distro  = line.split('=').last.strip if line =~ /^DISTRIB_ID=/
-          release = line.split('=').last.strip if line =~ /^DISTRIB_RELEASE=/
+          distro   = line.split('=').last.strip if line =~ /^DISTRIB_ID=/
+          release  = line.split('=').last.strip if line =~ /^DISTRIB_RELEASE=/
+          codename = line.split('=').last.strip if line =~ /^DISTRIB_CODENAME=/
         end
-      else
-        if debian_version.stdout.chomp =~ /^[[:digit:]]+\.[[:digit:]]+$/
-          release = debian_version.stdout.chomp
+      elsif (lsb_release = run_command("cat /etc/os-release")) && lsb_release.success?
+        lsb_release.stdout.each_line do |line|
+          distro   = line.split('=').last.delete('"').strip if line =~ /^ID=/
+          release  = line.split('=').last.delete('"').strip if line =~ /^VERSION_ID=/
+          codename = line.split('=').last.delete('"').strip if line =~ /^VERSION_CODENAME=/
+        end
+        # There is no codename notation until Debian Jessie
+        if codename.nil?
+          lsb_release.stdout.each_line do |line|
+            version = line.split('=').last.delete('"').strip if line =~ /^VERSION=/
+            # For debian releases
+            if m = /^[0-9]+ \((\w+)\)$/.match(version)
+              codename = m[1]
+            end
+          end
         end
       end
       distro ||= 'debian'
-      release ||= nil
-      { :family => distro.gsub(/[^[:alnum:]]/, '').downcase, :release => release }
+      # lsb-release not available or reported no version number:
+      if release.nil? || release == 'n/a'
+        release = case debian_version.stdout.chomp
+                  when /^[[:digit:]]+\.[[:digit:]]+$/
+                    debian_version.stdout.chomp
+                  when %r{^\w+/sid$}
+                    # a number larger than any normal Debian version ever:
+                    2**32 - 1.0
+                  else
+                    nil
+                  end
+      end
+      { :family => distro.gsub(/[^[:alnum:]]/, '').downcase, :release => release, :codename => codename }
     end
   end
 end
